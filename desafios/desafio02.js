fbad5e5e73df4f4a90c9071e7100ba9d081ae0c8
Gabriel Godoy <gabrielgdy090@users.noreply.github.com>
2023-12-09 11:34:16 -0300

    Aula 045 - Criei boolean.js

---
+++ desafios/desafio02.js
@@ -0,0 +1,15 @@
+//PI * raio * raio
+// Constantes usam letras maiusculas
+
+let raio = 10
+const PI = 3.14
+let areaCirc = PI * raio * raio
+
+console.log("Área é igual: " + PI * raio * raio + " m2" )
+
+console.log("O valor da área é " + areaCirc + " m2")
+
+console.log(Math.PI)
+
+areaCirc = Math.PI * raio * raio
+console.log("O valor da área é " + areaCirc + " m2")
---
+++ fundamentos/tipagemfraca.js
@@ -0,0 +1,8 @@
+let qua
+
+
+//Evitar nomes genéricos e siglas
+
+let valor = ''
+let numero = 2
+console.log(numero)
